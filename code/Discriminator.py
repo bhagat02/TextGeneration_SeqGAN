57d3f1b67166f33bf57e17c95f05242231ca1f28
bhagat02 <30953931+bhagat02@users.noreply.github.com>
2017-10-12 14:32:22 +0200

    Update Discriminator.py

--- code/Discriminator.py
+++ code/Discriminator.py
@@ -2,21 +2,9 @@ import tensorflow as tf
 import numpy as np
 
 # An alternative to tf.nn.rnn_cell._linear function, which has been removed in Tensorfow 1.0.1
-# The highway layer is borrowed from https://github.com/mkroutikov/tf-lstm-char-cnn
+# I have borrowed the highway layer from https://github.com/mkroutikov/tf-lstm-char-cnn
 def linear(input_, output_size, scope=None):
-    '''
-    Linear map: output[k] = sum_i(Matrix[k, i] * input_[i] ) + Bias[k]
-    Args:
-    input_: a tensor or a list of 2D, batch x n, Tensors.
-    output_size: int, second dimension of W[i].
-    scope: VariableScope for the created subgraph; defaults to "Linear".
-  Returns:
-    A 2D Tensor with shape [batch x output_size] equal to
-    sum_i(input_[i] * W[i]), where W[i]s are newly created matrices.
-  Raises:
-    ValueError: if some of the arguments has unspecified or wrong shape.
-  '''
-
+   
     shape = input_.get_shape().as_list()
     if len(shape) != 2:
         raise ValueError("Linear is expecting 2D arguments: %s" % str(shape))
@@ -63,9 +51,7 @@ class Discriminator(object):
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-        # Keeping track of l2 regularization loss (optional)
-        l2_loss = tf.constant(0.0)
-        
+      
         with tf.variable_scope('discriminator'):
 
             # Embedding layer
@@ -76,7 +62,7 @@ class Discriminator(object):
                 self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
                 self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
-            # Create a convolution + maxpool layer for each filter size
+            # Creating a convolution + maxpool layer for each filter size
             pooled_outputs = []
             for filter_size, num_filter in zip(filter_sizes, num_filters):
                 with tf.name_scope("conv-maxpool-%s" % filter_size):
@@ -87,26 +73,21 @@ class Discriminator(object):
                     conv = tf.nn.conv2d(
                         self.embedded_chars_expanded,
                         W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
                         name="conv")
-                    # Apply nonlinearity
+                    # Applying the nonlinearity
                     h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
                     # Maxpooling over the outputs
                     pooled = tf.nn.max_pool(
                         h,
                         ksize=[1, sequence_length - filter_size + 1, 1, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
                         name="pool")
                     pooled_outputs.append(pooled)
-            
-            # Combine all the pooled features
+            #we are combining all the pooled features
             num_filters_total = sum(num_filters)
             self.h_pool = tf.concat(pooled_outputs, 3)
             self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
-            # Add highway
+            # Add highway to prevent decaying of the learning rate
             with tf.name_scope("highway"):
                 self.h_highway = highway(self.h_pool_flat, self.h_pool_flat.get_shape()[1], 1, 0)
 
@@ -114,7 +95,7 @@ class Discriminator(object):
             with tf.name_scope("dropout"):
                 self.h_drop = tf.nn.dropout(self.h_highway, self.dropout_keep_prob)
 
-            # Final (unnormalized) scores and predictions
+          
             with tf.name_scope("output"):
                 W = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
@@ -124,7 +105,7 @@ class Discriminator(object):
                 self.ypred_for_auc = tf.nn.softmax(self.scores)
                 self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
-            # CalculateMean cross-entropy loss
+            # Calculating Mean cross-entropy loss
             with tf.name_scope("loss"):
                 losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
                 self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
